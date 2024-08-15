f2a7e0bae0230c100a9b487ab3c70c794e29bbe8
SiddharthPant <spsiddharthpant@gmail.com>
2024-08-16 02:11:09 +0530

    Switch to LazilyRefreshDatabase and reorganize tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase as LazilyRefreshDatabaseAlias;
 use Tests\TestCase;
 
 /*
@@ -14,7 +14,7 @@ use Tests\TestCase;
 |
 */
 
-uses(TestCase::class, RefreshDatabase::class)->in('Feature');
+uses(TestCase::class, LazilyRefreshDatabaseAlias::class)->in('Feature');
 
 /*
 |--------------------------------------------------------------------------
